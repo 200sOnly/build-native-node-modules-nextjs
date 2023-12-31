9f7273bc47ee0b885a177f4250986976fac2fdb5
200sOnly <abhitech6.0@gmail.com>
2023-12-31 09:20:48 -0500

    clean up project structure

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const { interpolateName } = require("loader-utils");
+
+const schema = require("./options.json");
+
+export default function loader(content) {
+  const { rootContext, _compiler, getOptions, emitFile } = this;
+  const options = getOptions(schema);
+  const { flags, outputPath, includeWebpackPublicPath } = options;
+
+  const isWebpackPathIncluded = includeWebpackPublicPath || false;
+  const name = interpolateName(this, "[name].[ext]", {
+    context: rootContext,
+    content,
+  });
+
+  emitFile(name, content);
+
+  return `
+try {
+  process.dlopen(module, ${JSON.stringify(
+    outputPath || _compiler.options.output.path
+  )} + require("path").sep + ${isWebpackPathIncluded ? '__webpack_public_path__' : '""'} + ${JSON.stringify(name)}${
+    typeof flags !== "undefined" ? `, ${JSON.stringify(options.flags)}` : ""
+  });
+} catch (error) {
+  throw new Error('nextjs-node-loader:\\n' + error);
+}
+`;
+}
+
+export const raw = true;
