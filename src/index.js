06b56d1a2a871b0876061640d4f138bf0ccecec1
Abhishek Mishra <53316702+200sOnly@users.noreply.github.com>
2024-01-08 11:46:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ export default function loader(content) {
   });
 
   emitFile(name, content);
-
+  let xx = `${JSON.stringify(outputPath || _compiler.options.output.path)}`
+  xx = xx.slice(0, -1) + '/' + xx.slice(-1)
+  console.log(`$$$$$$$$$$$$`);
+  console.log(xx);
+  console.log(`&&&&&&&&&&&`);
   return `
 try {
-  process.dlopen(module, ${JSON.stringify(
-    outputPath || _compiler.options.output.path
-  )} + require("path").sep + ${isWebpackPathIncluded ? '__webpack_public_path__' : '""'} + ${JSON.stringify(name)}${
-    typeof flags !== "undefined" ? `, ${JSON.stringify(options.flags)}` : ""
-  });
+  process.dlopen(module, ${xx} + ${isWebpackPathIncluded ? '__webpack_public_path__' : '""'} + ${JSON.stringify(name)}${typeof flags !== "undefined" ? `, ${JSON.stringify(options.flags)}` : ""});
 } catch (error) {
   throw new Error('nextjs-node-loader:\\n' + error);
 }
